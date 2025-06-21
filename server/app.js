d7bc742dcf02b253cfcd6315e9af8865d6a4180d
pylshrma <pylsharma.2310@gmail.com>
2025-06-21 19:48:10 +0530

    fixed port

--- server/app.js
+++ server/app.js
@@ -74,4 +74,7 @@ socket.on("disconnect",function()
 
 app.use("/",indexRouter);
 
-server.listen(3000);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
